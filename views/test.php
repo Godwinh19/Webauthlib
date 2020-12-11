4b1368eb66bd3232f7bbd3794eaa61621f491fdb
EZENGOLD <noudekiki@gmail.com>
2020-12-11 14:14:45 +0100

    fetch modifications

--- views/test.php
+++ views/test.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <title>Take picture using webcam</title>
     <link rel="stylesheet" href="../assets/css/bootstrap4.min.css">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="../assets/style.css">
     <link rel="stylesheet" href="../assets/fontawesome5/css/all.css">
 </head>
@@ -40,8 +39,6 @@
     </div>
     <script src="../assets/jquery/jquery.min.js"></script>
     <script src="../assets/jquery/bootstrap.min.js"></script>
-    <!-- <script src="https://co de.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> -->
     <script src="../assets/auth.js"></script>
 </body>
 
